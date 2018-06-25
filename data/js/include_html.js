40677c1c419de9b1164b4a6df6ff866deec9af97
Tim Müller <mullertim99@gmail.com>
2018-06-25 14:48:07 +0200

    Added HTML include script

---
+++ data/js/include_html.js
@@ -0,0 +1,32 @@
+
+// Include HTML into the current HTML file
+function include_html() {
+  var z = document.getElementsByTagName("*");
+  for (var i = 0; i < z.length; i++) {
+    var elem = z[i];
+    // Get the elements with the include tag
+    file = elem.getAttribute("html-file-path");
+    if (file) {
+      // Do an HTTP request for the file
+      console.log("Found tag, importing '" + file + "'");
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          console.log("HTTP request result: " + this.status)
+          console.log("Importing HTML...")
+          if (this.status == 200) {elem.innerHTML = this.responseText;}
+          else if (this.status == 404) {elem.innerHTML = "Page not found";}
+          else {elem.innerHTML = "Could not import HTML"}
+          // Remove the attribute, so no endless loops
+          elem.removeAttribute("html-file-path");
+          console.log("Done, looking for further includes")
+          include_html();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  console.log("No includes found")
+}
