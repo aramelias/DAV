d25631716b79001b590455024090f39218043bf9
Aram Elias <aramelias@hotmail.com>
2018-06-25 15:21:04 +0200

    More webdesign

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>DAV</title>
+    <title>DAV</title>
+    <link rel="stylesheet" href="layout.css" />
 </head>
+
 <body>
-Welcome to Data Analysis and Visualization.
+    <?php include("header.php") ?>
+
+    <div class="container">
+        <p>Welcome to Data Analysis and Visualization.</p>
+    </div>
+
+    <?php include("footer.php") ?>
 </body>
 </html>
