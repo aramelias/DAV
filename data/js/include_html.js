788914adae5de7e38fa8ebc44f80677ba0d0dc02
Tim Müller <mullertim99@gmail.com>
2018-06-25 14:55:03 +0200

    Added status description

--- data/js/include_html.js
+++ data/js/include_html.js
@@ -1,6 +1,8 @@
 
 // Include HTML into the current HTML file
 function include_html() {
+  ERROR_CODES = {0:"undef", 200:"OK", 404:"NotFound"}
+
   var z = document.getElementsByTagName("*");
   for (var i = 0; i < z.length; i++) {
     var elem = z[i];
@@ -12,7 +14,7 @@ function include_html() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          console.log("HTTP request result: " + this.status)
+          console.log("HTTP request result: " + this.status + " (" + ERROR_CODES[this.status] + ")")
           console.log("Importing HTML...")
           if (this.status == 200) {elem.innerHTML = this.responseText;}
           else if (this.status == 404) {elem.innerHTML = "Page not found";}
