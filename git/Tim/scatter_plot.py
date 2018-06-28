07e40c1d6679f9dd514a036bd6c1e21d4531e76c
Tim Müller <mullertim99@gmail.com>
2018-06-28 13:06:43 +0200

    Rice.html range update

--- git/Tim/scatter_plot.py
+++ git/Tim/scatter_plot.py
@@ -267,8 +267,8 @@ def do_graph_animation (graph_product, title, path, progress_bar):
 
     legend = bokeh.models.Legend(items=legend_list, location=(0,0), click_policy="mute")
 
-    f.x_range = bkm.DataRange1d(start=-0.5, end=3)
-    f.y_range = bkm.DataRange1d(start=18, end=30)
+    f.x_range = bkm.DataRange1d(start=0, end=3.6)
+    f.y_range = bkm.DataRange1d(start=19, end=32)
 
     # Make the slider
     callback = bokeh.models.CustomJS(args=dict(sources=sources, all_data=country_data), code=callback_code)
